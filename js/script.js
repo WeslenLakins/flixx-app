2a87a0b18a28859bceed6004903e13a41c074af6
Weslen Lakins <weslen.lakins@outlook.com>
2023-12-30 14:15:50 -0500

    Refactor TV show card display in script.js

--- js/script.js
+++ js/script.js
@@ -57,28 +57,28 @@ async function displayPopularShows() {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-    <a href="tv-details.html?id=${show.id}">
-      ${
-        show.poster_path
-          ? `<img
-        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
-        class="card-img-top"
-        alt="${show.name}"
-      />`
-          : `<img
-      src="../images/no-image.jpg" 
-      class="card-img-top" 
-      alt="${show.name}" 
-      />`
-      }
-    </a>
-    <div class="card-body">
-      <h5 class="card-title">${show.name}</h5>
-      <p class="card-text">
-        <small class="text-muted">Release: ${show.first_air_date}</small>
-      </p>
-    </div>
-    `;
+          <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${show.name}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            </p>
+          </div>
+        `;
 
     document.querySelector('#popular-shows').appendChild(div);
   });
